81d7809374d816bde12a758fb2c14f21d9c0f78b
aashi1408 <aashiv2525@gmail.com>
2025-12-30 20:40:03 +0530

    Add searchInsert function for binary search
    Implement binary search to find the insert position for a target in a sorted array.
    Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
    
    You must write an algorithm with O(log n) runtime complexity.
    
     
    
    Example 1:
    
    Input: nums = [1,3,5,6], target = 5
    Output: 2
    Example 2:
    
    Input: nums = [1,3,5,6], target = 2
    Output: 1
    Example 3:
    
    Input: nums = [1,3,5,6], target = 7
    Output: 4

---
+++ search_insert_pos.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target)
+    {
+        int index = nums.size();
+        int start = 0 , end = nums.size() - 1 , mid;
+
+        while(start<=end)
+        {
+            mid = (start+end)/2;
+            if(nums[mid]==target)
+            {
+                // index = mid;
+                return mid;
+            }
+            else if(nums[mid]<target)
+            {
+                start = mid + 1;
+            }
+            else if(nums[mid]>target)
+            {
+                index = mid;
+                end = mid - 1;
+            }
+        }
+        return index;
+    }
+};
